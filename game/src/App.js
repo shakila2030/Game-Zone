717a58c3f4946b6ecc29ce36ddfe74a8a448db80
RashiniNethmini <s.a.r.nethmini@gmail.com>
2023-06-29 23:58:17 +0530

    Memory Game added

--- game/src/App.js
+++ game/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import HomePage from "./Pages/HomePage/HomePage";
 import TicTacToe from "./Pages/TicTacToe/TicTacToe";
+import MemoryGame from "./Pages/MemoryGame/MemoryGame";
 
 function App() {
   return (
     <div>
      {/* <HomePage/> */}
-     <TicTacToe/>
+     {/* <TicTacToe/> */}
+     <MemoryGame/>
     </div>
   );
 }
