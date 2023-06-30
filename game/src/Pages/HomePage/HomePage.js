80dae7e39039947acaa4c92492a04c56df5ee0b3
shakila2030 <105419083+shakila2030@users.noreply.github.com>
2023-06-30 14:58:49 +0530

    homepage added

--- game/src/Pages/HomePage/HomePage.js
+++ game/src/Pages/HomePage/HomePage.js
@@ -1,48 +1,91 @@
 import React from "react";
 import styles from "./HomePage.module.css";
-import astronautImg from '../../images/astronaut.jpg';
-import earth from '../../images/earth.jpg';
-
-
+import wallpaper from "../../images/wallpaper.png";
+import PersonAddAltIcon from "@mui/icons-material/PersonAddAlt";
+import LoginIcon from "@mui/icons-material/Login";
+import Button from "@mui/material/Button";
+import { IconButton } from "@mui/material";
+import { useState } from "react";
 
 function HomePage() {
+
+
+  const [hoveredButton, setHoveredButton] = useState(null);
+
+  const handleMouseEnter = (buttonName) => {
+    setHoveredButton(buttonName);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredButton(null);
+  };
+
+  const isButtonHovered = (buttonName) => {
+    return hoveredButton === buttonName;
+  };
+
   return (
-    <div>
-      <div id="adbox"></div>
-      <div id="footer">
-        <ul id="featured" className="wrapper clearfix">
-          <li>
-          <img src={astronautImg} alt="Astronaut" />
-            <h3><a href="blog.html">Category 1</a></h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec mi tortor. Phasellus commodo semper vehicula.
-            </p>
-          </li>
-          <li>
-            <img src={earth} alt="Img" height="204" width="220" />
-            <h3><a href="blog.html">Category 2</a></h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec mi tortor. Phasellus commodo semper vehicula.
-            </p>
-          </li>
-          <li>
-            <img src="images/spacecraft-small.jpg" alt="Img" height="204" width="220" />
-            <h3><a href="blog.html">Category 3</a></h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec mi tortor. Phasellus commodo semper vehicula.
-            </p>
-          </li>
-          <li>
-            <img src="images/space-shuttle.jpg" alt="Img" height="204" width="220" />
-            <h3><a href="blog.html">Category 4</a></h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec mi tortor. Phasellus commodo semper vehicula.
-            </p>
-          </li>
-        </ul>
+    <div
+      className={styles.imageContainer}
+      style={{
+        width: "100%",
+        height: "100vh",
+        backgroundImage: `url(${wallpaper})`,
+        backgroundSize: "100% 100%",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className={styles.buttonContainer}>
+        <Button
+          style={{
+            color: "white",
+            border: "1px solid white",
+            marginRight: "30px",
+            width: "200px",
+            height: "100px",
+            background: isButtonHovered("signup") ? "lightblue" : "transparent",
+            boxShadow: isButtonHovered("signup") ? "0px 2px 4px rgba(0, 0, 0, 0.2)" : "none",
+          }}
+          startIcon={<PersonAddAltIcon />}
+          onMouseEnter={() => handleMouseEnter("signup")}
+          onMouseLeave={handleMouseLeave}
+        >
+          Sign Up
+        </Button>
+        <Button
+          style={{
+            color: "white",
+            border: "1px solid white",
+            marginRight: "30px",
+            width: "200px",
+            height: "100px",
+            background: isButtonHovered("login") ? "lightblue" : "transparent",
+            boxShadow: isButtonHovered("login") ? "0px 2px 4px rgba(0, 0, 0, 0.2)" : "none",
+          }}
+          startIcon={<LoginIcon />}
+          onMouseEnter={() => handleMouseEnter("login")}
+          onMouseLeave={handleMouseLeave}
+        >
+          Log In
+        </Button>
+        <Button
+          style={{
+            color: "white",
+            border: "1px solid white",
+            width: "200px",
+            height: "100px",
+            background: isButtonHovered("guest") ? "lightblue" : "transparent",
+            boxShadow: isButtonHovered("guest") ? "0px 2px 4px rgba(0, 0, 0, 0.2)" : "none",
+          }}
+          startIcon={<LoginIcon />}
+          onMouseEnter={() => handleMouseEnter("guest")}
+          onMouseLeave={handleMouseLeave}
+        >
+          Play as Guest
+        </Button>
       </div>
     </div>
   );
 }
-
 export default HomePage;
